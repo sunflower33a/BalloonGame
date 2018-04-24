07a20be904d4e5dd43533fa0fff3e027894a94d9
sunflower33a <--global>
2018-04-24 15:52:28 -0400

    Implement game engine into GUI

---
+++ Style.py
@@ -0,0 +1,50 @@
+
+YellowButton_up = """QPushButton{
+                        color: grey;
+                        border-image: url(artwork/arrow-yel-1.png);
+                        border-top: 3px transparent;
+                        border-bottom: 3px transparent;
+                        border-right: 10px transparent;
+                        border-left: 10px transparent;
+                    }"""
+YellowButton_down = """QPushButton{
+                        color: white;
+                        border-image: url(artwork/arrow-yel-2.png);
+                        border-top: 3px transparent;
+                        border-bottom: 3px transparent;
+                        border-right: 10px transparent;
+                        border-left: 10px transparent;
+                    }"""
+PinkButton_up = """QPushButton{
+                        color: white;
+                        border-image: url(artwork/arrow-pi-1.png);
+                        border-top: 3px transparent;
+                        border-bottom: 3px transparent;
+                        border-right: 10px transparent;
+                        border-left: 10px transparent;
+                }"""
+PinkButton_down = """QPushButton{
+                        color: white;
+                        border-image: url(artwork/arrow-pi-2.png);
+                        border-top: 3px transparent;
+                        border-bottom: 3px transparent;
+                        border-right: 10px transparent;
+                        border-left: 10px transparent;
+                    }"""
+
+PlayButton = """QPushButton{
+                        color: white;
+                        border-image: url(artwork/Butt-PLAY.png);
+                        border-top: 3px transparent;
+                        border-bottom: 3px transparent;
+                        border-right: 10px transparent;
+                        border-left: 10px transparent;
+                    }""" + """QPushButton: pressed{
+                        color: white;
+                        border-image: url(artwork/arrow-pi-2.png);
+                        border-top: 3px transparent;
+                        border-bottom: 3px transparent;
+                        border-right: 10px transparent;
+                        border-left: 10px transparent;
+                    }"""
+
